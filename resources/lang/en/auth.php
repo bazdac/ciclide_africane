15f88f5bd16ac9487e4f3b0db30c8852526cbc13
bazdac <zapoliny@gmail.com>
2020-06-16 15:17:08 +0300

    login

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Date de conectare invalide',
+    'throttle' => 'Prea multe incercari de autentificare. Incercati  in :seconds secunde.',
+
+];
